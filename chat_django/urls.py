dda3184a5ef4dcad995d9ade02f0328a2e3f4e84
Mehran Jalili <mehranj73@gmail.com>
2021-03-25 21:42:09 +0430

    account view

--- chat_django/urls.py
+++ chat_django/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 from personal.views import (
     home_screen_view
@@ -31,6 +31,7 @@ from account.views import (
 
 urlpatterns = [
     path('', home_screen_view, name='home'),
+    path('account/', include('account.urls'), name='account'),
     path('admin/', admin.site.urls),
 
     path('login/', login_view, name="login"),
