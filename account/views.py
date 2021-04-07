01d61816e90ba9d4a2a76e0d8939b83ec3a50133
mehranj73 <41204086+mehranj73@users.noreply.github.com>
2021-04-07 19:31:16 +0430

    querying friends

--- account/views.py
+++ account/views.py
@@ -3,13 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
 from django.conf import settings
 
-from django.core.files.storage import default_storage
 from django.core.files.storage import FileSystemStorage
 import os
 import cv2
 import json
 import base64
-import requests
 from django.core import files
 
 from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
@@ -31,9 +29,16 @@ def account_search_view(request, *args, **kwargs):
                 username__icontains=search_query).distinct()
             user = request.user
             accounts = []  # [(account1, True), (account2, False), ...]
-            for account in search_results:
-                accounts.append((account, False))  # you have no friends yet
-            context['accounts'] = accounts
+            if user.is_authenticated:
+                # get the authenticated users friend list
+                auth_user_friend_list = FriendList.objects.get(user=user)
+                for account in search_results:
+                    accounts.append((account, auth_user_friend_list.is_mutual_friend(account)))
+                context['accounts'] = accounts
+            else:
+                for account in search_results:
+                    accounts.append((account, False))
+                context['accounts'] = accounts
 
     return render(request, "account/search_results.html", context)
 
