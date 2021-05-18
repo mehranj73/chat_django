198a3d678d262ef08f47944d26f5a88db6b47124
mehranj73 <41204086+mehranj73@users.noreply.github.com>
2021-05-19 00:32:03 +0430

    private chat consumer

--- chat_django/routing.py
+++ chat_django/routing.py
@@ -4,12 +4,14 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path, re_path
 
 from public_chat.consumers import PublicChatConsumer
+from chat.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter([
                 path('public_chat/<room_id>/', PublicChatConsumer.as_asgi()),
+                path('chat/<room_id>/', ChatConsumer.as_asgi()),
             ])
         )
     ),
