382348bc1065aa00b16070dbc5f8d29b045ddc7c
mehranj73 <41204086+mehranj73@users.noreply.github.com>
2021-05-31 23:33:24 +0430

    joining and leaving a chatroom

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,14 +2,15 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.core.serializers import serialize
 from django.utils import timezone
+
 import json
 
 from chat.models import RoomChatMessage, PrivateChatRoom
 from friend.models import FriendList
 from account.utils import LazyAccountEncoder
 from chat.utils import calculate_timestamp
-from chat.constants import *
 from chat.exceptions import ClientError
+from chat.constants import *
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
@@ -96,6 +97,19 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             "join": str(room.id),
         })
 
+        if self.scope["user"].is_authenticated:
+            # Notify the group that someone joined
+            await self.channel_layer.group_send(
+                room.group_name,
+                {
+                    "type": "chat.join",
+                    "room_id": room_id,
+                    "profile_image": self.scope["user"].profile_image.url,
+                    "username": self.scope["user"].username,
+                    "user_id": self.scope["user"].id,
+                }
+            )
+
     async def leave_room(self, room_id):
         """
         Called by receive_json when someone sent a leave command.
@@ -138,8 +152,10 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         # Check they are in this room
         if self.room_id != None:
             if str(room_id) != str(self.room_id):
+                print("CLIENT ERRROR 1")
                 raise ClientError("ROOM_ACCESS_DENIED", "Room access denied")
         else:
+            print("CLIENT ERRROR 2")
             raise ClientError("ROOM_ACCESS_DENIED", "Room access denied")
 
         # Get the room and send to the group about it
@@ -165,6 +181,17 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         """
         # Send a message down to the client
         print("ChatConsumer: chat_join: " + str(self.scope["user"].id))
+        if event["username"]:
+            await self.send_json(
+                {
+                    "msg_type": MSG_TYPE_ENTER,
+                    "room": event["room_id"],
+                    "profile_image": event["profile_image"],
+                    "username": event["username"],
+                    "user_id": event["user_id"],
+                    "message": event["username"] + " connected.",
+                },
+            )
 
     async def chat_leave(self, event):
         """
@@ -172,6 +199,17 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         """
         # Send a message down to the client
         print("ChatConsumer: chat_leave")
+        if event["username"]:
+            await self.send_json(
+                {
+                    "msg_type": MSG_TYPE_LEAVE,
+                    "room": event["room_id"],
+                    "profile_image": event["profile_image"],
+                    "username": event["username"],
+                    "user_id": event["user_id"],
+                    "message": event["username"] + " disconnected.",
+                },
+            )
 
     async def chat_message(self, event):
         """
