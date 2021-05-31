02b678de18f0b543e82f979858b856a3f2c1ba95
mehranj73 <41204086+mehranj73@users.noreply.github.com>
2021-06-01 00:11:46 +0430

    querying private chatroom messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,13 +2,14 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.core.serializers import serialize
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 import json
 
 from chat.models import RoomChatMessage, PrivateChatRoom
 from friend.models import FriendList
 from account.utils import LazyAccountEncoder
-from chat.utils import calculate_timestamp
+from chat.utils import calculate_timestamp, LazyRoomChatMessageEncoder
 from chat.exceptions import ClientError
 from chat.constants import *
 
@@ -47,7 +48,15 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
                     raise ClientError(422, "You can't send an empty message.")
                 await self.send_room(content["room"], content["message"])
             elif command == "get_room_chat_messages":
-                pass
+                await self.display_progress_bar(True)
+                room = await get_room_or_error(content['room_id'], self.scope["user"])
+                payload = await get_room_chat_messages(room, content['page_number'])
+                if payload != None:
+                    payload = json.loads(payload)
+                    await self.send_messages_payload(payload['messages'], payload['new_page_number'])
+                else:
+                    raise ClientError(204, "Something went wrong retrieving the chatroom messages.")
+                await self.display_progress_bar(False)
             elif command == "get_user_info":
                 room = await get_room_or_error(content['room_id'], self.scope["user"])
                 payload = get_user_info(room, self.scope["user"])
@@ -237,6 +246,14 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         """
         print("ChatConsumer: send_messages_payload. ")
 
+        await self.send_json(
+            {
+                "messages_payload": "messages_payload",
+                "messages": messages,
+                "new_page_number": new_page_number,
+            },
+        )
+
     async def send_user_info_payload(self, user_info):
         """
         Send a payload of user information to the ui
@@ -317,3 +334,25 @@ def get_user_info(room, user):
 @database_sync_to_async
 def create_room_chat_message(room, user, message):
     return RoomChatMessage.objects.create(user=user, room=room, content=message)
+
+
+@database_sync_to_async
+def get_room_chat_messages(room, page_number):
+    try:
+        qs = RoomChatMessage.objects.by_room(room)
+        p = Paginator(qs, DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE)
+
+        payload = {}
+        messages_data = None
+        new_page_number = int(page_number)
+        if new_page_number <= p.num_pages:
+            new_page_number = new_page_number + 1
+            s = LazyRoomChatMessageEncoder()
+            payload['messages'] = s.serialize(p.page(page_number).object_list)
+        else:
+            payload['messages'] = "None"
+        payload['new_page_number'] = new_page_number
+        return json.dumps(payload)
+    except Exception as e:
+        print("EXCEPTION: " + str(e))
+        return None
